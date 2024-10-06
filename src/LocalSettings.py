cc874ccc77b62824f49774ba623388af0ac3353e
jansenduarte1 <jansenduarte.dev@gmail.com>
2024-10-06 10:00:57 -0300

    Fix first decryption error
    Corrected the path for first decryption to run smoothly

--- src/LocalSettings.py
+++ src/LocalSettings.py
@@ -5,7 +5,7 @@ class LocalSettings:
     Win_User_Path = os.path.join(os.environ['USERPROFILE'])
 
     Enc_File_Path = "C:\\Jansen-PastaDados\\Important Info.sef"
-    Dec_File_Path = os.path.join(Win_User_Path, 'OneDrive\\Desktop\\Important Info.txt')
+    Dec_File_Path = "C:\\Jansen-PastaDados\\Important Info.txt"
 
     Internal_Data_Folder = "File_Encrypt_PY"
     Internal_Data_Path = os.path.join(os.path.join(os.environ['USERPROFILE']), "AppData\\Local\\" + Internal_Data_Folder)
