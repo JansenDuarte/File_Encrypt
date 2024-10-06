c113ee982f36845afc92a25c2a2470ea5f2811e7
jansenduarte1 <jansenduarte.dev@gmail.com>
2024-10-06 10:12:19 -0300

    Add information to LocalSetting.py
    Important notes to file names and how to use this program

--- src/LocalSettings.py
+++ src/LocalSettings.py
@@ -4,9 +4,13 @@ class LocalSettings:
 
     Win_User_Path = os.path.join(os.environ['USERPROFILE'])
 
+    #Path to save the encrypted file. File extension can be anything you want, withing reason of couse
     Enc_File_Path = "C:\\Jansen-PastaDados\\Important Info.sef"
+
+    #Path to retrieve the actual file you want to encrypt.
     Dec_File_Path = "C:\\Jansen-PastaDados\\Important Info.txt"
 
+    #Path to your private key. Not the safest, but we only want to keep some info from those pesky kids.
     Internal_Data_Folder = "File_Encrypt_PY"
     Internal_Data_Path = os.path.join(os.path.join(os.environ['USERPROFILE']), "AppData\\Local\\" + Internal_Data_Folder)
     Key_Name = "JD_Enc.usk"
