f3dbcb7edafb2acfc3724d80a91beef3ee6acfba
jansenduarte1 <jansenduarte.dev@gmail.com>
2024-10-06 10:54:11 -0300

    Fix LocalSettings.py
    Location to my file was in the code.... That would not help anyone

--- src/LocalSettings.py
+++ src/LocalSettings.py
@@ -5,10 +5,10 @@ class LocalSettings:
     Win_User_Path = os.path.join(os.environ['USERPROFILE'])
 
     #Path to save the encrypted file. File extension can be anything you want, withing reason of couse
-    Enc_File_Path = "C:\\Jansen-PastaDados\\Important Info.sef"
+    Enc_File_Path = "C:\\My_Folder\\My_File.sef"
 
     #Path to retrieve the actual file you want to encrypt.
-    Dec_File_Path = "C:\\Jansen-PastaDados\\Important Info.txt"
+    Dec_File_Path = "C:\\My_Folder\\My_File.txt"
 
     #Path to your private key. Not the safest, but we only want to keep some info from those pesky kids.
     Internal_Data_Folder = "File_Encrypt_PY"
