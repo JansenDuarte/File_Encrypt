b906204e4f348a5eb86fd2d347b702332cb9f2e1
jansenduarte1 <jansenduarte.dev@gmail.com>
2025-06-13 13:26:22 -0300

    Fix file paths
    Fixes file path names to be generic for better understanding

--- src/LocalSettings.py
+++ src/LocalSettings.py
@@ -3,20 +3,20 @@ import locale
 
 class LocalSettings:
 
-    #INFO: Remember not so send the changes in file names to git! You can change all the shit you want! But don't send you file location out
+    
 
     Win_User_Path = os.path.join(os.environ['USERPROFILE'])
 
     #Path to save the encrypted file. File extension can be anything you want, withing reason of couse
-    Enc_File_Path = "C:\\Jansen-PastaDados\\Important Info.sef"
+    Enc_File_Path = "C:\\My_Folder\\My_File.sef"
 
     #Path to retrieve the actual file you want to encrypt.
-    Dec_File_Path = "C:\\Jansen-PastaDados\\Important Info.txt"
+    Dec_File_Path = "C:\\My_Folder\\My_File.txt"
 
     #Path to your private key. Not the safest, but we only want to keep some info from those pesky kids.
     Internal_Data_Folder = "File_Encrypt_PY"
     Internal_Data_Path = os.path.join(os.path.join(os.environ['USERPROFILE']), "AppData\\Local\\" + Internal_Data_Folder)
-    Key_Name = "JD_Enc.usk"
+    Key_Name = "My_Key.usk"
     Key_Path = os.path.join(os.path.join(os.environ['USERPROFILE']), "AppData\\Local\\" + Internal_Data_Folder + "\\" + Key_Name)
 
     #Get system encoding to prevent w/r problems
